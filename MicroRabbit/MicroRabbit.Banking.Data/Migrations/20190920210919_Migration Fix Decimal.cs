ac006bb7ae25bd4d6bdf68fd0e989945d90c5bc4
bholmes111 <bholmes111@gmail.com>
2019-09-20 18:00:36 -0400

    Partially completed banking microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Migrations/20190920210919_Migration Fix Decimal.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MicroRabbit.Banking.Data.Migrations
+{
+    public partial class MigrationFixDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AccountBalance",
+                table: "Accounts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountBalance",
+                table: "Accounts",
+                nullable: true,
+                oldClrType: typeof(decimal));
+        }
+    }
+}

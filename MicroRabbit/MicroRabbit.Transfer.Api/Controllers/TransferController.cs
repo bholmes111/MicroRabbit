646ea353e127b2d637d93af0a7ea630a91246c06
bholmes111 <bholmes111@gmail.com>
2019-09-21 16:17:02 -0400

    Refactor RabbitMQ bus for dependency injection for parameterless constructors in our event handlers

---
+++ MicroRabbit/MicroRabbit.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Transfer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private readonly ITransferService _transferService;
+
+        public TransferController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<TransferLog>> Get()
+        {
+            return Ok(_transferService.GetTransferLogs());
+        }
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
